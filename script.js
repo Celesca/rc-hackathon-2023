c9acc03d9812613267280d5666ec7d1696c2995d
Sawit Koseeyaumporn | Folk <121187906+Celesca@users.noreply.github.com>
2023-11-02 00:16:06 +0700

    Backup beta version 2

--- script.js
+++ script.js
@@ -3,3 +3,24 @@ const toggler = document.querySelector("#sidebar-btn");
 toggler.addEventListener("click", function() {
     document.querySelector("#sidebar").classList.toggle("collapsed");
 })
+
+// Login Section
+
+const profileElement = document.getElementById('profile');
+
+const userIcon = document.getElementById('user-icon');
+const loginButton = document.getElementById('login-icon');
+
+
+const checkLogin=()=> {
+    if (profileElement.classList.contains('login')) {
+        userIcon.style.display = "block";
+        loginButton.style.display = "none";
+    }
+    else { 
+        userIcon.style.display = "none";
+        loginButton.style.display = "block";
+    }
+}
+
+checkLogin();
